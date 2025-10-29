7de426c02dd6cbc9c956f14ef211d0112538ef9f
MostafaMemari <m.memari711@gmail.com>
2025-10-29 23:21:24 +0330

    refactor: update development mode handling and improve response for local bot

--- api/index.ts
+++ api/index.ts
@@ -5,9 +5,14 @@ const isDev = process.env.NODE_ENV === "development";
 
 if (isDev) {
   console.log("🤖 Running in development mode with polling...");
-  bot.start(); // فقط polling
+  bot.start(); // لوکال
 } else {
   console.log("🚀 Running in production mode with webhook...");
 }
 
-export default isDev ? undefined : webhookCallback(bot, "https");
+export default isDev
+  ? (req: any, res: any) => {
+      res.statusCode = 200;
+      res.end("Bot is running in development mode");
+    }
+  : webhookCallback(bot, "https");
